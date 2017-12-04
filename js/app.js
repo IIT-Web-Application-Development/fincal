71674a69b58106c196778f2b336d05c2205f5329
Uriel Duran <uduran12@gmail.com>
2017-12-04 15:45:09 -0600

    forgot to init var

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $('#navbar').load('navbar.html');
 /*Tip Calc Stuff*/
 var bill;
 var splitTotal;
+var tipPercent;
 
 document.getElementById("bill-total").addEventListener("keyup", function() {/*When user finished typing*/
     bill = document.getElementById("bill-total").value;/*Take in value*/
