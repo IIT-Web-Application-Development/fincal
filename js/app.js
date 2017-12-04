db0de60019af3bd7b1cb80257dfee342f0b44aa0
Uriel Duran <uduran12@gmail.com>
2017-12-04 15:39:46 -0600

    init vars, get split total

--- js/app.js
+++ js/app.js
@@ -2,7 +2,12 @@
 $('#navbar').load('navbar.html');
 
 /*Tip Calc Stuff*/
+var bill;
+var splitTotal;
 
 document.getElementById("bill-total").addEventListener("keyup", function() {/*When user finished typing*/
     bill = document.getElementById("bill-total").value;/*Take in value*/
+    calculate();
   });
+
+document.getElementById("split-amount").innerHTML = splitTotal;
